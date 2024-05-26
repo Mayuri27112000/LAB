92358ce6dc0ca60f8ac7a8a81c847ec0f9587c96
Mayuri27112000 <169136898+Mayuri27112000@users.noreply.github.com>
2024-05-26 19:41:51 +0530

    Add files via upload

---
+++ ConvertToUpperCase.java
@@ -0,0 +1,10 @@
+public class ConvertToUpperCase {
+    public static void main(String[] args) {
+        
+        String lowerString = "java";
+        String upperCaseString = lowerString.toUpperCase();
+        
+        
+        System.out.println(upperCaseString);
+    }
+}
