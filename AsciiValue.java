892cbf9a435e8581b42a7b5d16adb72b14e980da
Mayuri27112000 <169136898+Mayuri27112000@users.noreply.github.com>
2024-05-26 19:41:04 +0530

    Add files via upload

---
+++ AsciiValue.java
@@ -0,0 +1,12 @@
+public class AsciiValue  
+{  
+public static void main(String[] args)   
+{  
+
+char ch1 = 'm';  
+
+int asciivalue1 = ch1;
+System.out.println("The ASCII value of m is: " + asciivalue1);  
+  
+}  
+}  
